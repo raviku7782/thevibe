bf0ae0aef954a7426de0e45427a5756aa0023023
Ravi Gupta <ravikumar2381616@gmail.com>
2024-08-28 17:41:33 +0530

    first commit

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:theviberestaurant@gmail.com">theviberestaurant@gmail.com</a>
+         <a href="mailto:santosh18041@gmail.com">santosh18041@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>10:00am to 11:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Kurji,Vikash Nagar, Patna, india - 800013</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="Mob:7782946066">+91 7782946066</a>
+         <a href="Mob:9534203823">+91 9534203823</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>The Vibe Restaurant</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
